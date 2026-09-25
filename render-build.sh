8bbd06675bfbb7a7befec46fbd3db39efbe5d0c3
nabila1406@gmail.com <nabilarosiana@gmail.com>
2026-09-25 12:48:58 +0800

    update script render build

--- render-build.sh
+++ render-build.sh
@@ -9,9 +9,12 @@ composer install --no-dev --optimize-autoloader
 npm install
 npm run build
 
+# Buat file database sqlite secara otomatis
+touch database/database.sqlite
+
 # Clear dan cache konfigurasi Laravel agar performa lebih cepat
 php artisan config:cache
 php artisan route:cache
 
-# Jalankan migrasi database otomatis
+# Jalankan migrasi database ke SQLite
 php artisan migrate --force
